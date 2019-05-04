f49f5cd986caf3f196f7a706d92fbfccddd360ec
Thomas Andreo <thomas.andreo@gmail.com>
2019-05-03 22:34:59 -0400

    Basic ? button

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MasterButton from '../MasterButton';
+import './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MasterButton />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MasterButton/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="MasterButton" />
+    );
+  }
+}
+
+export default App;
